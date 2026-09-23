48b7d73e67bec954f625d49b4757b9ab73d2ab91
seanchangg <changsean1234@gmail.com>
2026-09-22 22:15:10 -0400

    Init

--- com1.asm
+++ com1.asm
@@ -1,69 +1,69 @@
-BASE equ 0x3F8
+COM1_PORT equ 0x3F8
 
 com1_init: 
-    mov dx, BASE+3
+    mov dx, COM1_PORT+3
     in al, dx
     and al, 0x7F ; 01111111 - leftmost is most significant bit. This masks out bit 7 AKA DLAB bit
     out dx, al
     
-    mov dx, BASE+1
+    mov dx, COM1_PORT+1
     mov al, 0
     out dx, al ; DLAB bit is guaranteed reset, now we disable interrupts
  
-    mov dx, BASE+3
+    mov dx, COM1_PORT+3
     in al, dx
     or al, 0x80 ; 10000000 - set DLAB to 1
     out dx, al 
     
     ; now we're gonna set baud rate - we're going for dividing by 1 (00000001)
-    mov dx, BASE
+    mov dx, COM1_PORT
     mov al, 0x01
     out dx, al
-    mov dx, BASE+1
+    mov dx, COM1_PORT+1
     mov al, 0x00
     out dx, al
         
     ;set DLAB back to 0
-    mov dx, BASE+3
+    mov dx, COM1_PORT+3
     in al, dx
     and al, 0x7F 
     out dx, al
     
-    mov dx, BASE+3
+    mov dx, COM1_PORT+3
     in al, dx; about to set no parity & 1 stop bit. Parity is bits 3-5, to set none we just need 3 to be 0. stop bits is bit 2. SO we need xxxx00xx
     and al, 0xF3 ; 11110011 = 15, 3
     or al, 0x03 ; 00000011 - writes 1 1 to data bits telling it 8 bit word length
     out dx, al
     
-    mov dx, BASE+2
+    mov dx, COM1_PORT+2
     mov al, 0x07 ; 00000111 - clear and reset fifo and enable
     out dx, al
     ret
     
 serial_putc: 
-    mov dx, BASE+5 ; This accesses the line status register. bit 5 says if there's room for data
+    mov dx, COM1_PORT+5 ; This accesses the line status register. bit 5 says if there's room for data
     .wait: 
         in al, dx ; load for arithmetic
         test al, 0x20 ; check if we have room. This checks bit 5 (00100000)
         jz .wait ; if not, keep waiting
-	lodsb ; if so load in
-	mov dx, BASE
+	mov dx, COM1_PORT
+	mov al, bl
 	out dx, al ; put the character in the transmission buffer
 	ret ; returns after code is run to the caller code, otherwise cpu keeps executing into serial_print
 	
 	
 serial_print:
     .wait: 
-        mov dx, BASE+5 ; This accesses the line status register. bit 5 says if there's room for data
+        mov dx, COM1_PORT+5 ; This accesses the line status register. bit 5 says if there's room for data
         in al, dx ; load for arithmetic
         test al, 0x20 ; check if we have room. This checks bit 5 (00100000)
         jz .wait ; if not, keep waiting
-        lodsb ; load byte (one char) - looks at ds:si (16-bit) or ds:esi (32) and loads into al, ax, eax respectively. increments according to DF (direction flag) for moving up or down memory
-	    test al, al 
-	    jz .stop 
-	    mov dx, BASE
-	    out dx, al
-	    jmp .wait ; repeat
+    lodsb ; load byte (one char) - looks at ds:si (16-bit) or ds:esi (32) and loads into al, ax, eax respectively. increments according to DF (direction flag) for moving up or down memory
+	test al, al 
+	jz .stop 
+	mov dx, COM1_PORT
+	out dx, al
+	jmp .wait ; repeat
 	.stop:
 	   ret
     
--- rom.asm
+++ rom.asm
@@ -1,30 +1,118 @@
 bits 16
-org 0x000
+org 0x0000
+COM1_PORT equ 0x3F8
 
-start: ; after the far jump that sends us back to the low segment
+init: ; after the far jump that sends us back to the low segment
     cli ; close interrupt
     cld ; clear direction flag
     
 	xor ax, ax ; sets ax to 0
+	mov es, ax ; set 0 base for RAM
 	mov ss, ax ; ss = stack base memory. (the bottom of the stack)
 	mov sp, 0x7000 ; sp = stack top pointer - the current top of stack. 0-0x7c00 is free, so it starts there
 	
 	mov ax, 0xF000
-	mov ds, ax ; now it points to the correct base 
+	mov ds, ax ; now it points to the correct base for lodsb
+
+zerobda:
+	mov di, 0x400
+	mov cx, 0x80
+	xor ax, ax
+	rep stosw ; reads from di for initial address and cx for # of iters, decs til 0. Copies ax
+
+init_com1:
+	; probe and initiate COM1
+	mov dx, COM1_PORT+7
+	mov al, 0xAA
+	out dx, al
+	in al, dx
+	cmp al, 0xAA ; comp, write to zero flag
+	jne init_ivt ; jump not equal
+	mov word [es:0x400], COM1_PORT
 	
-	call com1_init
-	mov si, msg
-	call serial_print
+init_ivt:
+	mov ax, 0xF000 ; ROM memory base
+	mov dx, dummy_handler ; moves memory location of dummy_handler into dx. Since we're org0x00, this is without the ROM offset
+	mov cx, 0x00 ; starts at 0, loop until 256
+	.loop:
+		mov di, cx
+		shl di, 1 ; shift left 2 bits AKA multiply by 4
+		shl di, 1
+		mov [es:di], dx; little endian, so label comes "first"
+		mov [es:di + 2], ax
+		
+		inc cx
+		cmp cx, 0x0100
+		jne .loop
+	
+	mov di, 0x40 ; 0x10 * 4 = 60 = 0x40
+	mov ax, display_handler
+	stosw ; auto incs di by 2 bytes
+	mov ax, 0xF000
+	stosw
+	
+	mov di, 0x58 ; 0x16 * 4 = 88 = 0x58
+	mov ax, keyboard_handler
+	stosw ; auto incs di by 2 bytes
+	mov ax, 0xF000
+	stosw
+	
+	sti
+		
+test:
+	mov al, 'A'
+	mov ah, 0x0e
+	int 0x10
 
-.hang:
+
+hang:
 	hlt
-	jmp .hang
+	jmp hang
+	
+dummy_handler:
+	iret
 
-msg: db "BIOS is... ALIVE!", 13, 10, 0
+display_handler: ; int 0x10. com1 putc uses bl to hold the character
+	push bx
+	cmp ah, 0x0e
+	jne .end
+	mov bl, al
+	call serial_putc
+	.end:
+		pop bx
+		iret
+
+keyboard_handler:
+	mov dx, 0x3FD ; line status register. bit 1 is 1 if data ready, 0 if not
+	push ax
+	
+	cmp ah, 0x01
+	je .nowait
+	
+	.wait:
+		in al, dx
+		test al, 1 ; check if data ready. if not, ZF is set to 0
+		jz .wait
+	
+	.nowait:
+		in al, dx
+		test al, 1 ; check if data ready. if not, ZF is set to 0
+		jz .end
+		
+	mov dx, 0x3F8
+	in al, dx
+	
+	mov ah, 0x0e
+	int 0x10
+	
+	.end:
+		pop ax
+		iret
 
 %include "com1.asm"
+msg: db "BIOS is... alive!!", 13, 10, 0
 times 0xFFF0-($-$$) db 0
 
-jmp 0xF000:start
+jmp 0xF000:init
 
 times 0x10000-($-$$) db 0
